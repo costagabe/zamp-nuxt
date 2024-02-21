7a80f865d317eea377a403977d8cf563c1d1eedf
Gabriel Costa <devgabcosta@gmail.com>
2024-02-21 01:54:22 -0300

    Atualizações de dependências e middleware de autenticação

--- ui/middleware/authentication.global.ts
+++ ui/middleware/authentication.global.ts
@@ -3,10 +3,12 @@ const publicRoutes = ["/login", "/register"];
 export default defineNuxtRouteMiddleware((to, from) => {
   const { authenticated } = storeToRefs(useAuthStore());
 
-  if(to.path === from.path) return;
+  if (to.path === from.path && authenticated.value) return;
 
   $fetch("/api/auth", { method: "GET" }).catch(() => {
+    console.log("User is not authenticated");
     authenticated.value = false;
+    console.log("User is not authenticated");
   });
 
   if (publicRoutes.includes(to.path)) {
