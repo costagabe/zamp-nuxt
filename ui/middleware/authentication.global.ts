60e62dc0b4ca2925fbb41a840053ca39dacb944f
Gabriel Costa <devgabcosta@gmail.com>
2024-02-22 17:04:43 -0300

    chore: authentiaction middleware improved

--- ui/middleware/authentication.global.ts
+++ ui/middleware/authentication.global.ts
@@ -3,17 +3,26 @@ const publicRoutes = ["/login", "/register"];
 export default defineNuxtRouteMiddleware((to, from) => {
   const { authenticated, level } = storeToRefs(useAuthStore());
 
-  if (to.path === from.path && authenticated.value && level.value !== null) return;
+  console.log("to", to.path, "from", from.path, "level", level.value);
+
+  if (
+    to.path === from.path &&
+    authenticated.value &&
+    to.path !== "/" &&
+    from.path !== "/" &&
+    level.value !== null
+  )
+    return;
 
   $fetch("/api/auth", { method: "GET" })
-  .then((res) => {
-    level.value = res as number;
-  })
-  .catch(() => {
-    console.log("User is not authenticated");
-    authenticated.value = false;
-    console.log("User is not authenticated");
-  });
+    .then((res) => {
+      level.value = res as number;
+    })
+    .catch(() => {
+      console.log("User is not authenticated");
+      authenticated.value = false;
+      console.log("User is not authenticated");
+    });
 
   if (publicRoutes.includes(to.path)) {
     if (authenticated.value) {
