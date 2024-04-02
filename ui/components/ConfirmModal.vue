8d8b3d3a6c291be78d126ed699507a9fe666be2b
Gabriel Costa <devgabcosta@gmail.com>
2024-04-02 17:34:33 -0300

    chore: delete modal UI update

--- ui/components/ConfirmModal.vue
+++ ui/components/ConfirmModal.vue
@@ -3,14 +3,6 @@
 
   const [model] = defineModel<boolean>("modelValue", { default: false });
 
-  const background = "bg-red-600";
-  const classes = {
-    body: { background },
-    header: { background },
-    footer: { background },
-    divide: "'divide-y divide-red-400 dark:divide-red-400",
-  };
-
   function handleConfirm() {
     model.value = false;
     emit("confirm");
@@ -19,7 +11,7 @@
 
 <template>
   <UModal v-model="model">
-    <u-card :ui="classes">
+    <u-card >
       <template #header>
         <h3 class="text-xl text-white font-semibold">Tem certeza que deseja continuar?</h3>
       </template>
@@ -27,9 +19,9 @@
       <template #footer>
         <div class="flex justify-end space-x-4">
           <u-button
-            @click="model = false"
+            @click="() => model = false"
             class="bg-green-600"
-            color=""
+            color="green"
           >
             <b class="text-white">Cancelar</b>
           </u-button>
